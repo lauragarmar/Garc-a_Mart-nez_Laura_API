576a2b58977d5ed1f150e677573f5d085e9db07f
laura <laura@laura.com>
2021-02-02 18:06:05 +0100

    ya estan hechos los dos fetch

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
 let cuerpo= document.getElementById("cuerpo");
 
-fetch("http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?date=1527811200&opacity=0.9&fill_bound=true&appid={API key}")
+/*  fetch("https://calendarific.com/api/v2/holidays?&api_key=16f912c9625c58a56df8017b58c31bdf64da6c87&country=ES&year=2021")
 .then(datos => datos.json())
 .then(json => {
     //console.log(datos_json);
-    mostrar(json)
+    mostrar(json);
+})
+
+const mostrar = (json) => {
+console.log(json);
+
+}  */
+
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Malaga&amp&APPID=14e95eae08bdcd44f298164d48a2ad66")
+.then(datos => datos.json())
+.then(json => {
+    //console.log(datos_json);
+    mostrar(json);
 })
 
 const mostrar = (json) => {
 console.log(json);
 
 }
-
+ 
 
 
 
