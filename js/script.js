ba75885b899ec3c05deeda3fd7a7d04a618a647b
Laura García <lauragarmar94@gmail.com>
2021-02-03 16:59:50 +0100

    Septimo commit. Intentando que me funcione el segundo select.

--- js/script.js
+++ js/script.js
@@ -3,18 +3,10 @@ let container = document.getElementById("container");
 let country=document.getElementById("country");
 let search=document.getElementById("search");
 let holiday=document.getElementById("holiday");
+let article= document.getElementById("article");
+//tengo que hacer una variable con el selected??
 
-/*  fetch("https://calendarific.com/api/v2/holidays?&api_key=16f912c9625c58a56df8017b58c31bdf64da6c87&country=ES&year=2021")
-.then(datos => datos.json())
-.then(json => {
-    //console.log(datos_json);
-    mostrar(json);
-})
 
-const mostrar = (json) => {
-console.log(json);
-
-}  */
 
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Malaga&amp&APPID=14e95eae08bdcd44f298164d48a2ad66")
     .then(datos => datos.json())
@@ -33,8 +25,27 @@ const mostrar = (json) => {
 
 }
 
-document.addEventListener("DOMContentLoaded", mostrar);
+const research= ()=>{
+    fetch("https://calendarific.com/api/v2/holidays?&api_key=16f912c9625c58a56df8017b58c31bdf64da6c87&country=ES&year=2021")
+    .then(datos => datos.json())
+    .then(json => {
+        //console.log(datos_json);
+        result(json);
+    })
+    
+    const result = (json) => {
+    console.log(json);
+    let option = document.createElement("option");
+   
+   /*  option.value =;
+    option.textContent = ; */
+    country.appendChild(option);
+    
+    } 
+}
 
+document.addEventListener("DOMContentLoaded", mostrar);
+search.addEventListener("click", research);
 
 
 
