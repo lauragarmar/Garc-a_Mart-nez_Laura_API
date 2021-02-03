6259b75b1608908ee2c8559b6af005e1ec34fbe8
Laura García <lauragarmar94@gmail.com>
2021-02-03 16:43:04 +0100

    Sexto commit. Me funciona el primer select

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-let container= document.getElementById("container");
+let container = document.getElementById("container");
+
+let country=document.getElementById("country");
+let search=document.getElementById("search");
+let holiday=document.getElementById("holiday");
 
 /*  fetch("https://calendarific.com/api/v2/holidays?&api_key=16f912c9625c58a56df8017b58c31bdf64da6c87&country=ES&year=2021")
 .then(datos => datos.json())
@@ -13,17 +17,24 @@ console.log(json);
 }  */
 
 fetch("http://api.openweathermap.org/data/2.5/weather?q=Malaga&amp&APPID=14e95eae08bdcd44f298164d48a2ad66")
-.then(datos => datos.json())
-.then(json => {
-    //console.log(datos_json);
-    mostrar(json);
-})
+    .then(datos => datos.json())
+    .then(json => {
+        //console.log(datos_json);
+        mostrar(json);
+    })
 
 const mostrar = (json) => {
-console.log(json);
+    console.log(json);
+    let option = document.createElement("option");
+   
+    option.value =json.name;
+    option.textContent = json.name;
+    country.appendChild(option);
 
 }
- 
+
+document.addEventListener("DOMContentLoaded", mostrar);
+
 
 
 
