11706f2f4a3d75ae92282aa6e0cf4d1c258f17d6
Laura García <lauragarmar94@gmail.com>
2021-02-05 13:14:23 +0100

    Noveno commit, viendo como arreglar los fetch

--- js/script.js
+++ js/script.js
@@ -7,9 +7,11 @@ let article= document.getElementById("article");
 //tengo que hacer una variable con el selected??
 
 
+//fetch("https://pro.openweathermap.org/data/2.5/forecast/climate?zip=94040,us&appid=14e95eae08bdcd44f298164d48a2ad66")
+//fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html") 
+fetch("http://api.openweathermap.org/data/2.5/weather?q=Andalucia&amp&APPID=14e95eae08bdcd44f298164d48a2ad66")
 
-fetch("http://api.openweathermap.org/data/2.5/weather?q=Malaga&amp&APPID=14e95eae08bdcd44f298164d48a2ad66")
-    .then(datos => datos.json())
+.then(datos => datos.json())
     .then(json => {
         //console.log(datos_json);
         mostrar(json);
@@ -35,17 +37,26 @@ const research= ()=>{
     
     const result = (json) => {
     console.log(json);
-    let option = document.createElement("option");
+    /*let option = document.createElement("option");
    
-   /*  option.value =;
-    option.textContent = ; */
-    country.appendChild(option);
+     option.value =;
+    option.textContent = ; 
+    country.appendChild(option);*/
+    for (let pais of json.response.holidays) {
+        let option = document.createElement("option");
+        console.log(pais);
+        option.value =pais.country.name;
+        option.textContent =pais.country.name;
+        holiday.appendChild(option);
+
+    }
     
     } 
 }
 
+document.addEventListener("DOMContentLoaded", research);
+/* search.addEventListener("click", research); */
 document.addEventListener("DOMContentLoaded", mostrar);
-search.addEventListener("click", research);
 
 
 
